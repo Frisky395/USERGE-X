8f68167fd196abc3fc20907602019d64ce680f4c
Frisky395 <79519962+Frisky395@users.noreply.github.com>
2021-04-11 19:45:18 +0700

    Update ping.py

--- userge/plugins/tools/ping.py
+++ userge/plugins/tools/ping.py
@@ -46,7 +46,7 @@ async def pingme(message: Message):
     def ping_buttons():
         buttons = [
             [
-                InlineKeyboardButton(text="  UPTIME", =("{userge.uptime}"),
+                InlineKeyboardButton(text="  UPTIME", ="{userge.uptime}",
             ]
         ]
         return InlineKeyboardMarkup(buttons)
